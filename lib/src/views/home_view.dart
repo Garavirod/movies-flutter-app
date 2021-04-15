8c7281454d690e206a51733c22bcfc23e0bbc830
rodgaravi <rgarcia@atid.edu.mx>
2021-04-15 17:42:26 -0500

    swiper was added

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
@@ -7,12 +8,40 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Movies in cinema'),
+          backgroundColor: Colors.green[300],
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search), 
+              onPressed: () {}
+            )
+          ],
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              this._swiperCaards()
+            ],
           ),
+        )
+    );
+  }
+
+  /* Methods */
+  Widget _swiperCaards () {
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      padding: EdgeInsets.only(top:10.0),
+      child: Swiper(          
+          layout: SwiperLayout.STACK,
+          itemWidth: 300.0,
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("https://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
         ),
     );
   }
